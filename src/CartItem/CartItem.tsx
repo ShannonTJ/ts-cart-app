553223282853ef625f3f11b38660110a09a60b8f
Shannon TJ <shannontj@shaw.ca>
2021-04-03 00:07:11 -0600

    setup cart item component

---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+//types
+import { CartItemType } from "../App";
+//styles
+import { Wrapper } from "./CartItem.styles";
+
+const CartItem: React.FC = () => {
+  return <div></div>;
+};
+
+export default CartItem;
