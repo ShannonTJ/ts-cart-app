bf043cb959695a6ff08a500c8ffbdbde3802a394
Shannon TJ <shannontj@shaw.ca>
2021-04-03 00:30:36 -0600

    handle add to cart function

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,24 @@ const App = () => {
   const getTotalItems = (items: CartItemType[]) =>
     items.reduce((acc: number, item) => acc + item.amount, 0);
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      //check if item is already in the cart
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+
+      //if so, update the amount
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
+
   const handleRemoveFromCart = () => null;
 
   if (isLoading) return <LinearProgress />;
